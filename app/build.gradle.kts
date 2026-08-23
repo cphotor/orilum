d6c25172a58f2213562b7018591c854c9ff99386
Simba Ho <cphotor@icloud.com>
2026-08-23 14:31:07 +0800

    build: bump version to 0.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.orilum"
         minSdk = 23
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.2.1"
 
         vectorDrawables {
             useSupportLibrary = true
